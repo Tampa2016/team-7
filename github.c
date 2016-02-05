0c5d25828056a34dcfbacffe1094c8487daf5c8e
Chris <cgado12@gmail.com>
2016-02-05 15:42:21 -0500

    basic terminal git commands

---
+++ github.c
@@ -0,0 +1,19 @@
+// github notes to remember
+
+/*
+click pull request on website
+copy link
+go to terminal
+
+in terminal locate folder you want repository
+git command "git clone (paste)"   ->createes repository on local machine
+git status - shows difference between loacal and github
+git add (filename) - adds to folder  "-a" adds all to folder 
+git commit - commits things to machine to repository  -m "" adds a messege
+git push - sends things to gihub, publicezes to team.
+git pull - receive things from github
+
+branch, merge, and workflow...
+git branch shows the branches
+git branch (filename)   - creates a copy of the master branch
+*/
